973aa7660fd5ce2dad911aab5d89d2ffa7fe1476
faizah21 <faizahansar23@gmail.com>
2023-09-08 12:32:10 -0400

    Updated

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,46 +1,52 @@
-export async function login(authDetail){
+export async function login(authDetail) {
   const requestOptions = {
-      method: "POST",
-      headers: {"content-Type": "application/json"},
-      body: JSON.stringify(authDetail)
-  }
-  const response = await fetch("http://localhost:8000/login", requestOptions);
-  if(!response.ok){
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(authDetail),
+  };
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/login`,
+    requestOptions
+  );
+  if (!response.ok) {
     // eslint-disable-next-line
-      throw { message: response.statusText, status: response.status };
+    throw { message: response.statusText, status: response.status };
   }
   const data = await response.json();
 
-  if(data.accessToken){
-  sessionStorage.setItem("token", JSON.stringify(data.accessToken));
-  sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+  if (data.accessToken) {
+    sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+    sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
   }
 
   return data;
 }
 
-export async function register(authDetail){
+export async function register(authDetail) {
   const requestOptions = {
-      method: "POST",
-      headers: {"content-Type": "application/json"},
-      body: JSON.stringify(authDetail)
-  }  
-  const response = await fetch("http://localhost:8000/register", requestOptions);
-  if(!response.ok){
+    method: "POST",
+    headers: { "content-Type": "application/json" },
+    body: JSON.stringify(authDetail),
+  };
+  const response = await fetch(
+    `${process.env.REACT_APP_HOST}/register`,
+    requestOptions
+  );
+  if (!response.ok) {
     // eslint-disable-next-line
-      throw { message: response.statusText, status: response.status };
+    throw { message: response.statusText, status: response.status };
   }
   const data = await response.json();
-  
-  if(data.accessToken){
-  sessionStorage.setItem("token", JSON.stringify(data.accessToken));
-  sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+
+  if (data.accessToken) {
+    sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+    sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
   }
 
   return data;
 }
 
-export function logout(){
+export function logout() {
   sessionStorage.removeItem("token");
   sessionStorage.removeItem("cbid");
 }
