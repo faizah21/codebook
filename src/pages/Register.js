ce943408a5030d350b112accb4f115c26b0c3a04
faizah21 <faizahansar23@gmail.com>
2023-09-09 17:55:59 -0400

    Register Update

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -43,7 +43,7 @@ export const Register = () => {
             type="name"
             id="name"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Shubham Sarda"
+            placeholder="Faizah Ansar"
             required
             autoComplete="off"
           />
@@ -59,7 +59,7 @@ export const Register = () => {
             type="email"
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="shubham@example.com"
+            placeholder="faizah@gmail.com"
             required
             autoComplete="off"
           />
